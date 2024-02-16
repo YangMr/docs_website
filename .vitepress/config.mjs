1cd6c40d152388ff27b0aed4332437d812bb1429
yangmr <759168442@qq.com>
2024-02-16 16:33:32 +0800

    init

---
+++ .vitepress/config.mjs
@@ -0,0 +1,52 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "YangMr的文档网站",
+  description: "YangMr的文档网站",
+  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  themeConfig: {
+    // logo: "logo.svg",
+    // https://vitepress.dev/reference/default-theme-config
+    search: {
+      provider: "local",
+    },
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "项目",
+        items: [
+          { text: "智慧园区项目", link: "/markdown-examples" },
+          { text: "智慧园区可视化大屏项目", link: "/api-examples" },
+          { text: "搭建UI组件库", link: "/api-examples" },
+        ],
+      },
+      {
+        text: "面试题",
+        items: [
+          { text: "项目难点", link: "/markdown-examples" },
+          { text: "面试题话术", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    socialLinks: [{ icon: "github", link: "https://github.com/yangmr" }],
+
+    footer: {
+      message:
+        'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
+      copyright:
+        'Copyright © 2019-present <a href="https://github.com/yangmr">Evan You</a>',
+    },
+  },
+});
---
+++ index.md
@@ -0,0 +1,26 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "YangMr的文档站"
+  text: "构建知识库"
+  tagline: 好记性不如烂笔头
+  image:
+    src: background.svg
+  actions:
+    - theme: brand
+      text: 开始使用
+      link: /markdown-examples
+    - theme: alt
+      text: GitHub
+      link: /api-examples
+
+features:
+  - title: 🚀 小建议
+    details: 选择永远大于努力
+  - title: 💪 努力
+    details: 如果做一件事就努力把它做好
+  - title: 📚 思考
+    details: 学会思考，不要人云亦云
+---
