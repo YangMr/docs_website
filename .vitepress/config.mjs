a9449861ca69314b20edca8378180aa15791e6f7
yangmr <759168442@qq.com>
2024-02-21 19:10:12 +0800

    add rbac

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -30,8 +30,8 @@ export default defineConfig({
       {
         text: "面试题",
         items: [
-          { text: "项目难点", link: "/docs/difficulty/index" },
-          { text: "面试题话术", link: "/docs/question/index" },
+          { text: "项目难点", link: "/docs/difficulty/index.md" },
+          { text: "面试题话术", link: "/docs/question/index.md" },
         ],
       },
       {
